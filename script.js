5ff1298d1019ad53a2c9637136b5073017e9bf23
Gabriel Maes <gabrielmaes@hotmail.com>
2022-06-27 19:42:43 +0200

    updated script.js

--- script.js
+++ script.js
@@ -7,14 +7,52 @@ function calculate() {
     
     if (fsex[0].checked) { //man
         
-        var total = 88362 + (13397 * Number(txtw.value)) + (4799 * Number(txth.value)) - (5677 * Number(txta.value))
+        var total = 88.362 + (13.397 * Number(txtw.value)) + (4.799 * Number(txth.value)) - (5.677 * Number(txta.value))
+      
+        
     }
     
     else if (fsex[1].checked) { //woman
         
-        var total = 447593 + (9247 * Number(txtw.value)) + (3098 * Number(txth.value)) - (4330 * Number(txta.value))
+        var total = 447.593 + (9.247 * Number(txtw.value)) + (3.098 * Number(txth.value)) - (4.330 * Number(txta.value))
+       
     }
-    window.alert(`Your Basal Metabolic Rate is: ${total}.`)
-    //r.innerHTML = `Your Basal Metabolic Rate is: ${total}.`
+    //window.alert(`Your Basal Metabolic Rate is: ${total}.`)
+    r.innerHTML = `Your Basal Metabolic Rate is: ${total.toFixed(0)} kcal.`
    
 }
+
+function check() {
+var txtsports = window.document.getElementById('sports')
+var sports = txtsports.value
+var r = window.document.getElementById('sportsres')
+var img = document.createElement('img')
+img.setAttribute('id', 'foto')
+
+    if (sports === 'cycling') {
+        r.innerHTML = 'You chose Cycling'
+        img.src = 'cycling.jpeg'
+
+        
+    } else if (sports === 'running') { 
+        r.innerHTML = 'You chose Running'
+        img.src = 'running.jpeg'
+    } else if (sports === 'swimming'){
+        r.innerHTML = 'You chose Swimming'
+        img.src = 'swimming.jpeg'
+    } else if (sports === 'walking'){
+        r.innerHTML = 'You chose Walking'
+        img.src = 'walking.jpeg'
+    } else if (sports === 'weightlifting'){
+        r.innerHTML = 'You chose Weight Lifting'
+        img.src = 'weightlifting.jpeg'
+    } else {
+        r.innerHTML = 'You did not chose any exercise'
+        img.src = 'lazy.jpeg'
+    } 
+    r.appendChild(img)
+    r.style.textAlign = 'center'
+    
+    
+
+} 
